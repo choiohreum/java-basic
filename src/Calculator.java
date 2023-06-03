35e7bd6aec2cbfc05103b3ee2cfdbf5433747eab
choiohreum <dhfmaghkdwp@naver.com>
2023-06-03 23:02:17 +0900

    Calculator 작성 복습

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Calculator {
     static Scanner sc = new Scanner(System.in);
     public  static  void  main(String[] args) {
-        System.out.println("텍스트를 입력하세요");
+        System.out.println("텍스트를 입력하세요1");
         String str = sc.nextLine();
 
         System.out.println("내가 입력한 것은 : " +str);
