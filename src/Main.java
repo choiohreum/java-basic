e207046735211f880b8349d32e2bafdc78a73d0c
choiohreum <dhfmaghkdwp@naver.com>
2023-06-04 16:40:43 +0900

    백준 문제풀이연습

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-       int a = sc.nextInt();
-       int b = sc.nextInt();
-       System.out.print(a+b);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A*B);
+
     }
 }
